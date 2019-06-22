4cb443e6722e9e6c43e9691d93c0e81a1a7c3b23
Brian Small <brian@epicjrny.com>
2019-06-22 11:19:01 -0700

    Updates project name; removes orphaned code from server.ts.

--- src/server.ts
+++ src/server.ts
@@ -1,24 +1,11 @@
-import * as fs from 'fs';
 import * as amqp from 'amqplib';
 
 import config from './config';
 import logger from './logger';
-import {FileEventMessage, EventType} from './fileEventMessage';
 import {Channel, Connection} from "amqplib";
 
 logger.info(`Node runtime environment (NODE_ENV): ${process.env.NODE_ENV}.`);
 
-const watchConfig = {
-    usePolling: config.get('server.watchConfig.usePolling'),
-    interval: config.get('server.watchConfig.interval'),
-    binaryInterval: config.get('server.watchConfig.binaryInterval'),
-    depth: config.get('server.watchConfig.depth'),
-    awaitWriteFinish: {
-        stabilityThreshold: config.get('server.watchConfig.awaitWriteFinish.stabilityThreshold'),
-        pollInterval: config.get('server.watchConfig.awaitWriteFinish.pollInterval')
-    },
-    ignoreInitial: config.get('server.watchConfig.ignoreInitial')
-};
 
 let mqChannel: Channel;
 
