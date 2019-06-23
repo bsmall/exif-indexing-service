2cd53a56710826b11bfa568e81a0e7875ffb874f
Brian Small <brian@epicjrny.com>
2019-06-23 12:58:19 -0700

    Stubs out code that retrieves message from queue and returns acknowledgement to queue so that message is removed.

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import * as amqp from 'amqplib';
 
 import config from './config';
 import logger from './logger';
-import {Channel, Connection} from "amqplib";
+import {Channel, Connection, ConsumeMessage, Replies} from "amqplib";
 
 logger.info(`Node runtime environment (NODE_ENV): ${process.env.NODE_ENV}.`);
 
@@ -19,17 +19,59 @@ amqp.connect(config.get('server.messageQueueConfig.hostUrl'))
             .then((channel: Channel) => {
                 logger.info(`Created message queue channel.`);
                 mqChannel = channel;
+
+                mqChannel.assertQueue(config.get('server.messageQueueConfig.queueName'), {
+                    durable: true,
+                    exclusive: false
+                })
+                    .then(queueValue => {
+                        console.log(`Waiting for messages in ${config.get('server.messageQueueConfig.queueName')}.`);
+
+                        // Consume messages from queue.
+                        mqChannel.consume(config.get('server.messageQueueConfig.queueName'), (message: ConsumeMessage | null) => {
+
+                            // Convert message to JSON object.
+                            let messageObj = undefined;
+
+                            if (message) {
+                                // Convert message payload to object.
+                                messageObj = JSON.parse(message.content.toString());
+
+                                logger.debug(`Starting to process: ${messageObj.eventType} --> ${messageObj.path}`);
+
+
+                                // TODO: Write code to process the message and update ElasticSearch database.
+
+
+                                logger.debug(`Done processing: ${messageObj.eventType} --> ${messageObj.path}`);
+
+                                mqChannel.ack(message);
+                                logger.debug(`Message acknowledged and dequeued for ${messageObj.eventType} --> ${messageObj.path}.`);
+                            }
+
+
+                        }, {
+                            noAck: false
+                        })
+
+
+                    })
+                    .error(errorInfo => {
+                        // TODO: Handle error info
+                        logger.error(`Error trying to access queue. ${errorInfo}.`);
+                    })
             })
             .error((error: any) => {
                 // TODO: Handle any channel errors.
+                logger.error(`Error establishing channel to message queue. ${error}`);
             });
     })
     .error((error: any) => {
-        logger.error(`Error connecting to message queue. ${error}.`);
-        //TODO: Stop the service instead of killing node process.
-        process.exit(1);   
-    });
-
+            logger.error(`Error connecting to message queue. ${error}.`);
+            //TODO: Stop the service instead of killing node process.
+            process.exit(1);
+        }
+    );
 
 
 
